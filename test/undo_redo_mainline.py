b882a1324c11a2c6d0a2b2039128e712127bf4e6
Phil Hanna <ph1204@gmail.com>
2020-06-21 08:50:49 -0400

    Added comments to mainline

--- test/undo_redo_mainline.py
+++ test/undo_redo_mainline.py
@@ -9,39 +9,68 @@ except:
 
 
 class Mainline:
+    """ Demonstrates an undo/redo session """
 
     def __init__(self):
+        """ Creates the undoer """
         self.app = UndoRedo()
 
     def run(self):
+        """ Runs the session """
+
         history = []
         logentry = "init"
+
         while True:
+
+            # Log the current status
             status = str(self.app)
             logentry = logentry + ", " + status
             history.append(logentry)
+
+            # Prompt for input
             print(status)
             print("> ", end='')
-            line = input().upper()
-            if line.startswith("Q"):
+            line = input()
+
+            # Quit
+            if line.upper().startswith("Q"):
                 break
+
+            # Get the input tokens, ignoring blank lines
             logentry = line
             tokens = line.split()
             if len(tokens) == 0:
                 continue
-            first = tokens[0][0]
-            if first == "S":
+
+            # First token is the command (set, undo, redo, help)
+            first = tokens[0].upper()
+
+            # Help
+            if first.startswith("H"):
+                pass
+
+            # Set
+            elif first.startswith("S"):
                 if len(tokens) < 2:
                     print("Set to what?")
                     continue
                 value_to_set = tokens[1]
                 self.app.set_value(value_to_set)
-            elif first == "U":
+
+            # Undo
+            elif first.startswith("U"):
                 self.app.undo()
-            elif first == "R":
+
+            # Redo
+            elif first.startswith("R"):
                 self.app.redo()
+
+            # Unrecognized command
             else:
                 print(f'"{line}" ?')
+
+        # At end, print the history of this session
         for status in history:
             print(status)
 
